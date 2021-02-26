acba5b2bb0410c49d6654cc6fe948130947d1621
Alex Robinson <m.alexanderrobinson@yahoo.com>
2021-02-26 16:12:29 -0600

    scan for bluetooth devices after window activation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 
 import sys
 import gi
+import gatt
+import dbus
 
 gi.require_version('Gtk', '3.0')
 
@@ -8,6 +10,9 @@ from gi.repository import Gtk, Gio
 
 from .window import SigloWindow
 
+class AnyDeviceManager(gatt.DeviceManager):
+    def device_discovered(self, device):
+        print("Discovered [%s] %s" % (device.mac_address, device.alias()))
 
 class Application(Gtk.Application):
     def __init__(self):
@@ -19,9 +24,12 @@ class Application(Gtk.Application):
         if not win:
             win = SigloWindow(application=self)
         win.present()
-
+        manager = AnyDeviceManager(adapter_name='hci0')
+        manager.start_discovery()
+        manager.run()
 
 def main(version):
     app = Application()
     return app.run(sys.argv)
+
 
--- src/window.py
+++ src/window.py
@@ -1,7 +1,5 @@
 
 from gi.repository import Gtk
-import gatt
-import dbus
 
 @Gtk.Template(resource_path='/org/gnome/siglo/window.ui')
 class SigloWindow(Gtk.ApplicationWindow):
@@ -12,22 +10,4 @@ class SigloWindow(Gtk.ApplicationWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    @Gtk.Template.Callback()
-    def sync_button_clicked(self, widget):
-        pass
-        #print("Hello World")
-
-    @Gtk.Template.Callback()
-    def on_window_focus(self, widget, user_data):
-        print("Hello world!")
-        #manager = AnyDeviceManager(adapter_name='hci0')
-        #manager.start_discovery()
-        #manager.run()
-
-class AnyDeviceManager(gatt.DeviceManager):
-    def device_discovered(self, device):
-        print("Discovered [%s] %s" % (device.mac_address, device.alias()))
-
-
 
