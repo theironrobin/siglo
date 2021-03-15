02d355cc52ef90d97228d4d21cb9cdcd604a2b5b
Alex R <m.alexanderrobinson@yahoo.com>
2021-03-14 21:35:20 -0500

    fix changing BLE adapter bug

--- src/main.py
+++ src/main.py
@@ -6,6 +6,12 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 from .window import SigloWindow
 from .bluetooth import InfiniTimeManager
+from bleson import get_provider
+
+def get_adapter_name():
+    adapter = get_provider().get_adapter(0)
+    id = adapter.device_id
+    return "hci" + str(id)
 
 class Application(Gtk.Application):
     def __init__(self, manager):
@@ -25,7 +31,7 @@ class Application(Gtk.Application):
         self.manager.stop()
 
 def main(version):
-    manager = InfiniTimeManager(adapter_name='hci0')
+    manager = InfiniTimeManager(adapter_name=get_adapter_name())
     app = Application(manager)
     return app.run(sys.argv)
 
