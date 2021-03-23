dcb52642414be3014f43bad54c801c0aa7f494fe
Alex Robinson <m.alexanderrobinson@yahoo.com>
2021-03-22 21:36:31 -0500

    src/window.py: async OTA Update working

--- src/window.py
+++ src/window.py
@@ -1,9 +1,27 @@
 
-from gi.repository import Gtk
+import threading
+from gi.repository import Gtk, GObject
 from .bluetooth import InfiniTimeDevice
-#import lib.ble_legacy_dfu_controller as BLDC
 from .ble_legacy_dfu_controller import BleDfuControllerLegacy
 from .unpacker import Unpacker
+
+# calls f on another thread
+def async_call(f, on_done):
+    if not on_done:
+        on_done = lambda r, e: None
+
+    def do_call():
+        result = None
+        error = None
+
+        try:
+            result = f()
+        except Exception as err:
+            error = err
+
+        GObject.idle_add(lambda: on_done(result, error))
+    thread = threading.Thread(target = do_call)
+    thread.start()
 
 @Gtk.Template(resource_path='/org/gnome/siglo/window.ui')
 class SigloWindow(Gtk.ApplicationWindow):
@@ -17,9 +35,11 @@ class SigloWindow(Gtk.ApplicationWindow):
     bt_spinner = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
+        self.ble_dfu = None
         self.ota_file = None
         self.manager = None
         super().__init__(**kwargs)
+        GObject.threads_init()
 
     def done_scanning(self, manager):
         self.manager = manager
@@ -68,9 +88,6 @@ class SigloWindow(Gtk.ApplicationWindow):
 
     @Gtk.Template.Callback()
     def flash_it_button_clicked(self, widget):
-        self.main_info.set_text("Starting OTA Update...")
-        self.bt_spinner.set_visible(True)
-        self.ota_picked_box.set_visible(False)
         unpacker = Unpacker()
         try:
             hexfile, datfile = unpacker.unpack_zipfile(self.ota_file)	
@@ -78,33 +95,37 @@ class SigloWindow(Gtk.ApplicationWindow):
             print("ERR")
             print(e)
             pass
-        ble_dfu = BleDfuControllerLegacy(self.manager.get_mac_address(), hexfile, datfile)
-
-        ble_dfu.input_setup()
+        self.ble_dfu = BleDfuControllerLegacy(self.manager.get_mac_address(), hexfile, datfile)
+        self.ble_dfu.input_setup()
 
         # Connect to peer device. Assume application mode.
-        if ble_dfu.scan_and_connect():
-            if not ble_dfu.check_DFU_mode():
+        if self.ble_dfu.scan_and_connect():
+            if not self.ble_dfu.check_DFU_mode():
                 print("Need to switch to DFU mode")
-                success = ble_dfu.switch_to_dfu_mode()
+                success = self.ble_dfu.switch_to_dfu_mode()
                 if not success:
                     print("Couldn't reconnect")
         else:
             # The device might already be in DFU mode (MAC + 1)
-            ble_dfu.target_mac_increase(1)
+            self.ble_dfu.target_mac_increase(1)
 
             # Try connection with new address
             print("Couldn't connect, will try DFU MAC")
-            if not ble_dfu.scan_and_connect():
+            if not self.ble_dfu.scan_and_connect():
                 raise Exception("Can't connect to device")
-        GObject.threads_init()
-        Gio.io_scheduler_push_job(slow_stuff, None, GLib.PRIORITY_DEFAULT, None)
-        
-        ble_dfu.start()
-
+    
+        async_call(self.slow_load, self.slow_complete)
+    
+    def slow_complete(self, results, errors):
         # Disconnect from peer device if not done already and clean up.
-        ble_dfu.disconnect()
+        self.ble_dfu.disconnect()
         self.main_info.set_text("OTA Update Complete")
         self.bt_spinner.set_visible(False)
+    
+    def slow_load(self):
+        self.main_info.set_text("Starting OTA Update...")
+        self.bt_spinner.set_visible(True)
+        self.ota_picked_box.set_visible(False)
+        self.ble_dfu.start()
 
         
