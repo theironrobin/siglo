868b3462e41b851f181adbc797c8d82036515b2b
Alex <m.alexanderrobinson@yahoo.com>
2021-03-28 02:00:41 +0000

    src/main.py: open window before starting bluetooth manager

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ from .bluetooth import InfiniTimeManager
 
 
 class Application(Gtk.Application):
-    def __init__(self, manager):
-        self.manager = manager
+    def __init__(self):
+        self.manager = None
         super().__init__(
             application_id="org.gnome.siglo", flags=Gio.ApplicationFlags.FLAGS_NONE
         )
@@ -20,6 +20,7 @@ class Application(Gtk.Application):
         if not win:
             win = SigloWindow(application=self)
         win.present()
+        self.manager = InfiniTimeManager()
         self.manager.scan_for_infinitime()
         win.done_scanning(self.manager)
 
@@ -29,6 +30,5 @@ class Application(Gtk.Application):
 
 
 def main(version):
-    manager = InfiniTimeManager()
-    app = Application(manager)
+    app = Application()
     return app.run(sys.argv)
