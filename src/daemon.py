4d875df5c8bfd54a61b0bd0856720976bbf485f4
Alex Robinson <m.alexanderrobinson@yahoo.com>
2021-09-01 15:14:49 -0500

    fixup dbus monitoring for chatty notifications

--- src/daemon.py
+++ src/daemon.py
@@ -12,7 +12,7 @@ class daemon:
         self.conf = config()
         self.manager = InfiniTimeManager()
         self.device = InfiniTimeDevice(manager=self.manager, mac_address=self.conf.get_property("last_paired_device"))
-        self.device.connect(sync_time=False)
+        self.device.connect()
 
     def scan_for_notifications(self):
         DBusGMainLoop(set_as_default=True)
@@ -24,14 +24,28 @@ class daemon:
         mainloop = glib.MainLoop()
         mainloop.run()
 
+    def scan_for_notifications(self):
+        DBusGMainLoop(set_as_default=True)
+        monitor_bus = dbus.SessionBus(private=True)
+        try:
+            dbus_monitor_iface = dbus.Interface(monitor_bus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus'), dbus_interface='org.freedesktop.DBus.Monitoring')
+            dbus_monitor_iface.BecomeMonitor(["interface='org.freedesktop.Notifications', member='Notify'"], 0)
+        except dbus.exceptions.DBusException as e:
+            print(e)
+            return
+        monitor_bus.add_message_filter(self.notifications)
+        mainloop = glib.MainLoop()
+        mainloop.run()
+
     def notifications(self, bus, message):
         alert_dict = {}
         for arg in message.get_args_list():
             if isinstance(arg, dbus.Dictionary):
                 if arg["desktop-entry"] == "sm.puri.Chatty":
                     alert_dict["category"] = "SMS"
-                    alert_dict["sender"] = message.get_args_list()[3].split("New message from ")[1]
+                    alert_dict["sender"] = message.get_args_list()[3]
                     alert_dict["message"] = message.get_args_list()[4]
         alert_dict_empty = not alert_dict
         if len(alert_dict) > 0:
+            print(alert_dict)
             self.device.send_notification(alert_dict)
